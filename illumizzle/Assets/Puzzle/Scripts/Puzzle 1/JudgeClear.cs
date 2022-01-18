5eb97cf8689b1f7e744f2dc0453dbfe450db4da5
Seohyun Yang <seoh136199@gmail.com>
2022-01-18 15:11:16 +0900

    씬 이름 수정, 퍼즐1 스테이지 3, 4 수정, JudgeClear 수정

--- illumizzle/Assets/Puzzle/Scripts/Puzzle 1/JudgeClear.cs
+++ illumizzle/Assets/Puzzle/Scripts/Puzzle 1/JudgeClear.cs
@@ -3,34 +3,37 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class JudgeClear : MonoBehaviour {
-    /*
+
     public int cntTotFitTrigger;
     public int cntFitTrigger = 0;
+    private bool isFinished = false;
 
     void Update() {
-        if (cntFitTrigger == cntTotFitTrigger) {
+        if (isFinished == false && cntFitTrigger == cntTotFitTrigger) {
+            isFinished = true;
             Debug.Log("ÆÛÁñ Å¬¸®¾î!!");
+            PuzzleSystem.instance.AfterPuzzle(true);
         }
     }
-    */
 
-    public int cntTotFitTrigger;
 
-    private int _cntFitTrigger = 0;
-    public int cntFitTrigger
-    {
-        get
-        {
-            return _cntFitTrigger;
-        }set
-        {
-            _cntFitTrigger = value;
-            if(_cntFitTrigger == cntTotFitTrigger)
-            {
-                Debug.Log("ÆÛÁñ Å¬¸®¾î");
+    //public int cntTotFitTrigger;
 
-                PuzzleSystem.instance.AfterPuzzle(true);
-            }
-        }
-    }
+    //private int _cntFitTrigger = 0;
+    //public int cntFitTrigger
+    //{
+    //    get
+    //    {
+    //        return _cntFitTrigger;
+    //    }set
+    //    {
+    //        _cntFitTrigger = value;
+    //        if(_cntFitTrigger == cntTotFitTrigger)
+    //        {
+    //            Debug.Log("ÆÛÁñ Å¬¸®¾î");
+
+    //            PuzzleSystem.instance.AfterPuzzle(true);
+    //        }
+    //    }
+    //}
 }
