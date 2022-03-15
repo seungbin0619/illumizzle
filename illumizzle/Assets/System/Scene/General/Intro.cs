5acc388f85a70f1486d053aa6b9e5a36e7ded50a
seungbin0619 <seungbin0619@naver.com>
2022-03-15 23:21:10 +0900

    저장알림?

--- illumizzle/Assets/System/Scene/General/Intro.cs
+++ illumizzle/Assets/System/Scene/General/Intro.cs
@@ -6,6 +6,8 @@ public class Intro : MonoBehaviour
 {
     private void Start()
     {
+        DataSystem.SetData("Story", "Start", 1);
+
         ActionSystem.instance.AddAction(ActionSystem.Action.ActionType.Move, "Title");
         ActionSystem.instance.Play();
     }
